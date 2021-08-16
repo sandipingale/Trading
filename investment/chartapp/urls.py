11a50363f4142acafb4c27eb1550603b2849545f
sandipingale <55628344+sandipingale@users.noreply.github.com>
2021-08-16 14:53:07 +0530

    Add the graphs for moving average analysis

--- investment/chartapp/urls.py
+++ investment/chartapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('charts', views.charts, name='charts'),
-    path('chartdata',views.get_chart_data,name='chart-data')
+    path('chartdata',views.get_chart_data,name='chart-data'),
+    path('analysis',views.analysis,name='analysis')
 
 ]
--- investment/chartapp/views.py
+++ investment/chartapp/views.py
@@ -1,10 +1,85 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import yfinance as yf
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 # Create your views here.
 
 def charts(request):
     return render(request,'chartapp/chartbase.html',{})
+
+def analysis(request):
+    symbol = request.GET.get("symbol", None)
+    if symbol:
+        symbol = symbol
+    else:
+        symbol = 'NIFTYBEES.NS'
+
+
+    start_date = '2021-01-01'
+    end_date = '2021-08-16'
+    moving_average = 10
+    no_of_shares = 10
+    symbol = yf.Ticker(symbol)
+    hist = symbol.history(start=start_date, end=end_date)
+    df = hist
+    df = df.reset_index()
+    df['MA'] = df['Close'].rolling(window=moving_average).mean()
+    df['avg_diff'] = (df['Close'] - df['MA']) / df['MA'] * 100
+    df['inv_value'] = df['Close'] * -1 * no_of_shares
+    df['just_date'] = df['Date'].dt.date
+    print(df['just_date'].tolist())
+
+    response = {
+        'type': 'line',
+        'data':{
+            'labels': df['just_date'].tolist(),
+            'datasets': [
+                {
+                    'label': 'Average Difference',
+                    'data': df['avg_diff'].tolist(),
+                    'borderColor': 'rgb(75, 192, 192)',
+                    'fill': False,
+                    'tension': 0.5
+                }
+            ],
+        },
+        'options':{
+            'responsive': True,
+            'elements':{
+                'point':{
+                    'pointRadius':0.5
+                }
+            }
+        }
+    }
+
+    response1 = {
+        'type': 'line',
+        'data':{
+            'labels': df['just_date'].tolist(),
+            'datasets': [
+                {
+                    'label': 'Close Price',
+                    'data': df['Close'].tolist(),
+                    'borderColor': 'rgb(75, 192, 192)',
+                    'fill': False,
+                    'tension': 0.1
+                }
+            ],
+        },
+        'options':{
+            'responsive': True,
+            'elements':{
+                'point':{
+                    'pointRadius':0.1
+                }
+            }
+        }
+    }
+    #return response
+
+    return render(request,'chartapp/analysis.html',{'xxxx':json.dumps(response,cls=DjangoJSONEncoder),'yyyy':json.dumps(response1,cls=DjangoJSONEncoder)})
 
 def get_chart_data(request):
     chart_type = request.GET.get("type", None)
